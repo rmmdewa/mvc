8da7c478b0aead0e6a84382f886bdc4739d477c0
rmmdewa <rammdewa@gmail.com>
2022-09-10 11:11:43 +0700

    fitur simpan

---
+++ src/com/koneksi/koneksi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.koneksi;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Administrator
+ */
+public class koneksi {
+    private static Connection con;
+    public static Connection getcon() {
+    if (con == null) {
+        try {
+            String url = "jdbc:mysql://localhost/sekolahh"; //nama database
+            String username = "root";
+            String password = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+        return con;
+    }
+}
